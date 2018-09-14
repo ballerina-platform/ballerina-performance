5d3f8072bf364791b33c6723380fd251c1864f1d
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2018-09-14 14:08:19 +0530

    Wait for Ballerina port and use a relative path to find the Ballerina program

--- distribution/scripts/ballerina/ballerina-start.sh
+++ distribution/scripts/ballerina/ballerina-start.sh
@@ -17,24 +17,30 @@
 # Start Ballerina Service
 # ----------------------------------------------------------------------------
 
-bal_file=""
-heap_size=""
+ballerina_path=""
+ballerina_file=""
+default_heap_size="1g"
+heap_size="$default_heap_size"
 
 function usage() {
     echo ""
     echo "Usage: "
-    echo "$0 -b <bal_file> [-m <heap_size>] [-h] -- [ballerina_flags]"
+    echo "$0 -p <ballerina_path> -b <ballerina_file> [-m <heap_size>] [-h] -- [ballerina_flags]"
     echo ""
+    echo "-p: The Ballerina program path."
     echo "-b: The Ballerina program."
-    echo "-m: The heap memory size of Ballerina VM."
+    echo "-m: The heap memory size of Ballerina VM. Default: $default_heap_size."
     echo "-h: Display this help and exit."
     echo ""
 }
 
-while getopts "b:m:h" opts; do
+while getopts "p:b:m:h" opts; do
     case $opts in
+    p)
+        ballerina_path=${OPTARG}
+        ;;
     b)
-        bal_file=${OPTARG}
+        ballerina_file=${OPTARG}
         ;;
     m)
         heap_size=${OPTARG}
@@ -53,16 +59,20 @@ shift "$((OPTIND - 1))"
 
 bal_flags="$@"
 
-if [[ -z $bal_file ]]; then
+if [[ ! -d $ballerina_path ]]; then
+    echo "Please provide the Ballerina path."
+    exit 1
+fi
+
+if [[ ! -f $ballerina_path/$ballerina_file ]]; then
     echo "Please provide the Ballerina program."
     exit 1
 fi
 
 if [[ -z $heap_size ]]; then
-    heap_size="1G"
+    echo "Please provide the heap size for the Ballerina program."
+    exit 1
 fi
-
-ballerina_path=$HOME/ballerina/bal
 
 if pgrep -f ballerina.*/bre >/dev/null; then
     echo "Shutting down Ballerina"
@@ -75,9 +85,9 @@ fi
 
 log_files=(${ballerina_path}/logs/*)
 if [ ${#log_files[@]} -gt 1 ]; then
-    echo "Log files exists. Moving to /tmp/${bal_file}/"
-    mkdir -p /tmp/${bal_file}
-    mv ${ballerina_path}/logs/* /tmp/${bal_file}/
+    echo "Log files exists. Moving to /tmp/${ballerina_file}/"
+    mkdir -p /tmp/${ballerina_file}
+    mv ${ballerina_path}/logs/* /tmp/${ballerina_file}/
 fi
 
 echo "Setting Heap to ${heap_size}"
@@ -85,12 +95,15 @@ echo "Setting Heap to ${heap_size}"
 echo "Enabling GC Logs"
 export JAVA_OPTS="-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${ballerina_path}/logs/gc.log -Xms${heap_size} -Xmx${heap_size}"
 
-ballerina_command="ballerina run ${bal_flags} ${bal_file}"
+ballerina_command="ballerina run ${bal_flags} ${ballerina_file}"
 echo "Starting Ballerina: $ballerina_command"
 cd $ballerina_path
 nohup $ballerina_command &>${ballerina_path}/logs/ballerina.log &
 
 # TODO Do a curl and check if service is started
-echo "Waiting for 5 seconds to make sure that the server is ready to accept requests."
-sleep 5
+echo "Waiting to make sure that the server is ready to accept requests."
+while ! nc -zv localhost 9090; do
+    sleep 5
+done
+
 tail -10 ${ballerina_path}/logs/ballerina.log
