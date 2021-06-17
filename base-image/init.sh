60d97bffde0252e2cafc03816156d4540db29ac7
Anjana Supun <anjanas@wso2.com>
2021-06-17 11:50:57 +0530

    Add base image

---
+++ base-image/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+# Copyright 2021 WSO2 Inc. (http://wso2.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------------------------------------------------------
+# Installation script for the VM
+# ----------------------------------------------------------------------------
+set -e
+
+sudo apt-get update && sudo apt-get install openjdk-8-jdk -y
+echo '#!/bin/sh' | sudo tee -a /etc/profile.d/10-perf-vm.sh
+echo 'export PATH=$PATH:/base-image/utils/jtl-splitter/' | sudo tee -a /etc/profile.d/10-perf-vm.sh
+echo 'export PATH=$PATH:/base-image/utils/payloads/' | sudo tee -a /etc/profile.d/10-perf-vm.sh
+(cd /base-image/scripts/; ./start-jmeter.sh -i /base-image -d)
+chmod -R 777 /base-image
